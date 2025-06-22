dba6cdbaa4059668fe98a359249cb932a1b61ef1
Vitor Augusto Tibério <142597619+VitorTiberio@users.noreply.github.com>
2025-06-22 14:46:44 -0300

    Add files via upload

---
+++ OpenCV/reader.py
@@ -0,0 +1,16 @@
+import cv2 
+
+imagem = cv2.imread("foto_01.jpg",1)
+imagem_cinza = cv2.imread("foto_01.jpg",0)
+
+detector_facial = cv2.CascadeClassifier("Cascades/haarcascade_frontalface_default.xml")
+
+detection = detector_facial.detectMultiScale(imagem_cinza)
+
+for x,y,w,h in detection: 
+    cv2.rectangle(imagem, (x,y),(x + w,y+h),(0,255,0),3)
+
+cv2.imshow("Rostos detectados", imagem)
+cv2.waitKey(0)
+
+
